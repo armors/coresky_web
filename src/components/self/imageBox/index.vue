973b88777416d0756bbe9da61f51cb03c21a642b
EternalShallow <eternalShallow@163.com>
2022-12-02 19:19:42 +0800

    img-box

---
+++ src/components/self/imageBox/index.vue
@@ -0,0 +1,32 @@
+<template>
+	<el-image class="cover-image" placeholder="loading" :src="src" fit="cover">
+		<template v-slot:placeholder>
+			<el-skeleton class="placeholder-image" animated>
+				<template #template>
+					<el-skeleton-item class="nft-image-skeleton" variant="h3" />
+				</template>
+			</el-skeleton>
+		</template>
+		<template v-slot:error>
+			<div class="display-flex box-center error-image-box">
+				<el-image class="error-image" :src="require('@/assets/create-img/non-existent.png')" fit="contain"></el-image>
+			</div>
+		</template>
+	</el-image>
+</template>
+
+<script>
+	export default {
+		name: "index",
+		props: {
+			src: {
+				type: String,
+				default: ''
+			}
+		}
+	}
+</script>
+
+<style scoped>
+
+</style>
--- src/views/sections/NFTDetail.vue
+++ src/views/sections/NFTDetail.vue
@@ -3,20 +3,7 @@
     <div class="flex-center">
       <div class="page-left">
         <div class="detail-img-box">
-          <el-image class="cover-image" placeholder="loading" :src="tokenInfo.ckCollectionsInfoEntity.image" fit="cover">
-            <template v-slot:placeholder>
-              <el-skeleton class="placeholder-image" animated>
-                <template #template>
-                  <el-skeleton-item class="nft-image-skeleton" variant="h3" />
-                </template>
-              </el-skeleton>
-            </template>
-            <template v-slot:error>
-              <div class="display-flex box-center error-image-box">
-                <el-image class="error-image" :src="require('@/assets/create-img/non-existent.png')" fit="contain"></el-image>
-              </div>
-            </template>
-          </el-image>
+          <image-box :src="tokenInfo.ckCollectionsInfoEntity.image"></image-box>
         </div>
         <div class="card-wrap mt30">
           <div class="card-head">
